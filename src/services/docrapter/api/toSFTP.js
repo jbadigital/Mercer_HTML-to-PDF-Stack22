2ed761c927b0d07b62b3fcc4ae744ffac960ff8b
nstonekalido <nathan.stone@kalido.com.au>
2020-12-08 14:59:27 +1100

    updated PDF filename based on field in MSL DE

--- src/services/docrapter/api/toSFTP.js
+++ src/services/docrapter/api/toSFTP.js
@@ -20,7 +20,7 @@ module.exports = function(context) {
       });
 
       const client = new ET_Client(process.env.SFMC_CLIENTID, process.env.SFMC_CLIENTSECRET, null, { origin: process.env.SFMC_ORIGIN, authOrigin: process.env.SFMC_AUTHORIGIN, soapOrigin: process.env.SFMC_SOAPORIGIN, authOptions: { authVersion : 2, accountId : process.env.SFMC_ACCOUNTID, scope : process.env.SFMC_SCOPE, applicationType : 'server'}});
-      const props = ['PDF_Status','Communication_Name','BRAND_TYPE','Interaction_Name','GUID'];
+      const props = ['PDF_Status','Communication_Name','BRAND_TYPE','Interaction_Name','GUID','PDF_Document_Name'];
       const filter = {
         leftOperand: 'PDF_Status',
         operator: 'equals',
@@ -38,10 +38,13 @@ module.exports = function(context) {
         SFMC.Brand_Type = _.find(response.body.Results[0].Properties.Property,function(item){ return item.Name === 'BRAND_TYPE'; }).Value;
         SFMC.Interaction_Name = _.find(response.body.Results[0].Properties.Property,function(item){ return item.Name === 'Interaction_Name'; }).Value;
         SFMC.GUID_Code = _.find(response.body.Results[0].Properties.Property,function(item){ return item.Name === 'GUID'; }).Value;
+        SFMC.PDF_Document_Name = _.find(response.body.Results[0].Properties.Property,function(item){ return item.Name === 'PDF_Document_Name'; }).Value;
 
         var dt = DateTime.local();
         //let filename=SFMC.Communication_Name+'_'+context.result.download_id+'_'+dt.toFormat('yyyy-MM-dd')+'T'+dt.toFormat('HH-mm-ss')+'.pdf';
-        let filename = SFMC.Brand_Type + '-' + SFMC.Interaction_Name + '-' + dt.toFormat('yyyy-MM-dd')+'T'+dt.toFormat('HH-mm-ss') + '-' + SFMC.GUID_Code + '.pdf';
+        
+        //let filename = SFMC.Brand_Type + '-' + SFMC.Interaction_Name + '-' + dt.toFormat('yyyy-MM-dd')+'T'+dt.toFormat('HH-mm-ss') + '-' + SFMC.GUID_Code + '.pdf';
+        let filename = SFMC.PDF_Document_Name;
 
         (async function () {
           await context.app.settings.sftp.put(Buffer.from(pdf.data), context.app.settings.printmatrix[SFMC.Communication_Name].Destination_Directory+filename);

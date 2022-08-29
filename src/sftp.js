10e555938087301dd9adb36342ca07cd052c64a6
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2022-08-29 19:01:33 +1000

    update timeout

--- src/sftp.js
+++ src/sftp.js
@@ -12,7 +12,7 @@ module.exports = async function (app) {
     algorithms: {
       serverHostKey: ['ssh-rsa', 'ssh-dss']
     },
-    readyTimeout: 500000000
+    readyTimeout: 50000000000
   });
   await app.set('sftp', sftp);
 

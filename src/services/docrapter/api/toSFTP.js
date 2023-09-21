f0107c2a6fbde7921f8bdd4482e7f88647168d73
AdamRidgway <73258538+AdamRidgway@users.noreply.github.com>
2023-09-21 18:36:38 +1000

    Update toSFTP.js

--- src/services/docrapter/api/toSFTP.js
+++ src/services/docrapter/api/toSFTP.js
@@ -18,7 +18,7 @@ module.exports = function(context) {
         responseType: 'arraybuffer',
         responseEncoding: 'binary'
       });
-      console.log ( typeof pdf.data ==='string' ? 'step 1 - result: '+ pdf.data.length : 'step 1 - result: not a atring ' );
+      console.log ( typeof pdf.data.toString() ==='string' ? 'step 1 - result: '+ pdf.data.toString().length : 'step 1 - result: not a atring ' );
       console.log (context.result.download_url);
       
       console.log('step 1 - finish');

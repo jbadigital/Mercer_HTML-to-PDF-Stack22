c3257211283c313af11c344003259f9061b02916
AdamRidgway <73258538+AdamRidgway@users.noreply.github.com>
2023-09-27 09:22:33 +1000

    Update toSFTP.js

--- src/services/docrapter/api/toSFTP.js
+++ src/services/docrapter/api/toSFTP.js
@@ -18,8 +18,11 @@ module.exports = function(context) {
         responseType: 'arraybuffer',
         responseEncoding: 'binary'
       });
-      console.log ( typeof pdf.data.toString() ==='string' ? 'step 1 - result: '+ pdf.data.toString().length : 'step 1 - result: not a atring ' );
-      //console.log ( test ? 'step 1 - result: '+ pdf.data.toString('utf8').length : 'step 1 - result: not a string ' );
+      //console.log ( typeof pdf.data.toString() ==='string' ? 'step 1 - result: '+ pdf.data.toString().length : 'step 1 - result: not a atring ' );
+
+      var test=pdf.data.toString('utf8');
+      console.log ( test ? 'step 1 - result: '+ pdf.data.toString('utf8').length : 'step 1 - result: not a string ' );
+      
       console.log (context.result.download_url);
       
       console.log('step 1 - finish');

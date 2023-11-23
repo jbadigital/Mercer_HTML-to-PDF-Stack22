d6133eba00ce376dddd635c1305ae97e436c6d15
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2023-11-23 15:12:46 +1100

    Update sftp.js

--- src/sftp.js
+++ src/sftp.js
@@ -9,7 +9,7 @@ module.exports = async function (app) {
     port: process.env.FTP_PORT,
     username: process.env.FTP_USERNAME,
     password: process.env.FTP_PASSWORD,
-    debug: console.log,
+    //debug: console.log,
     algorithms: {
       serverHostKey: ['ssh-rsa', 'ssh-dss']
     },

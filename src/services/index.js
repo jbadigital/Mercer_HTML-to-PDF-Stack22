64b06306d08da329d2d5208e3d621c79fc731d95
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2024-01-31 10:46:42 +1100

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,14 @@
 const users = require('./users/users.service.js');
 const pdf = require('./pdf/pdf.service.js');
 const docrapter = require('./docrapter/docrapter.service.js');
+const pdf_uat = require('./pdf_uat/pdf_uat.service.js');
+const docrapter_uat = require('./docrapter_uat/docrapter_uat.service.js');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(pdf);
   app.configure(docrapter);
+  app.configure(pdf_uat);
+  app.configure(docrapter_uat);
 };

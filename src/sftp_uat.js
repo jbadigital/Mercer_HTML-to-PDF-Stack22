82b58fab07b8299e05d6b140409c75c5681f7d36
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2024-01-31 14:59:31 +1100

    Update sftp_uat.js

--- src/sftp_uat.js
+++ src/sftp_uat.js
@@ -1,6 +1,6 @@
 const SFTP = require('ssh2-sftp-client');
-const logger = require('./logger');
-const logger = require('./logger');
+const sftp_UAT = new SFTP();
+const logger_UAT = require('./logger');
 
 module.exports = async function (app) {
 
@@ -9,17 +9,18 @@ module.exports = async function (app) {
     port: process.env.FTP_PORT_UAT,
     username: process.env.FTP_USERNAME_UAT,
     password: process.env.FTP_PASSWORD_UAT,
-    // debug: console.log,
-    // algorithms: {
-    // serverHostKey: ['ssh-rsa', 'ssh-dss']
-    // },
-
+//    debug: console.log,
+//    algorithms: {
+//      serverHostKey: ['ssh-rsa', 'ssh-dss']
+//    },
     retries: 10,
     retry_factor: 2,
     retry_minTimeout: 2000,
     readyTimeout: 10000000
-    });
-
+  });
   await app.set('sftp_UAT', sftp_UAT);
-  logger.info('SFTP_UAT CONNECTED');
-  };
+
+  logger_UAT.info('SFTP_UAT CONNECTED');
+
+};
+

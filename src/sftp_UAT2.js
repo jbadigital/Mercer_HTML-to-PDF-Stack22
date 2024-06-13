d0b363fc2b5cf7c01d99dac1a2e4ab71eb052611
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2024-06-13 14:58:46 +1000

    Create sftp_UAT2.js

---
+++ src/sftp_UAT2.js
@@ -0,0 +1,24 @@
+const SFTP = require('ssh2-sftp-client');
+const sftp = new SFTP();
+const logger = require('./logger');
+
+module.exports = async function (app) {
+
+  await sftp.connect({
+    host: process.env.FTP_HOST_URL_UAT2,
+    port: 22,
+    username: process.env.FTP_USERNAME_UAT2,
+    privateKey: Buffer.from(process.env.FTP_PRIVATEKEY_UAT2, 'base64').toString('ascii'),
+    //debug: console.log,
+    retries: 10,
+    retry_factor: 2,
+    retry_minTimeout: 2000,
+    readyTimeout: 10000000,
+    //keepaliveInterval: 72000000, // 20 hours Interval
+    //keepaliveCountMax: 3
+  });
+  await app.set('sftp', sftp);
+
+  logger.info('SFTP CONNECTED');
+
+};

e5a76386ec23bc6873b6a5920c7c83a460010d05
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2024-06-27 08:05:34 +1000

    Update sftp_uat2.js

--- src/sftp_uat2.js
+++ src/sftp_uat2.js
@@ -10,10 +10,19 @@ module.exports = async function (app) {
     username: process.env.FTP_USERNAME_UAT2,
     privateKey: Buffer.from(process.env.FTP_PRIVATEKEY_UAT2, 'base64').toString('ascii'),
     //debug: console.log,
-    keepaliveInterval: 60000
+    readyTimeout: 20000, // integer How long (in ms) to wait for the SSH handshake
+    strictVendor: true, // boolean - Performs a strict server vendor check
+    retries: 2, // integer. Number of times to retry connecting
+    retry_factor: 2, // integer. Time factor used to calculate time between retries
+    retry_minTimeout: 2000, // integer. Minimum timeout between attempts
+    promiseLimit: 10 // max concurrent promises for downloadDir/uploadDir
+    //keepaliveInterval: 60000
   });
   await app.set('sftp_UAT2', sftp_UAT2);
 
   logger.info('SFTP_UAT2 CONNECTED');
 
+  setInterval(function() {
+    var t = sftp.stat('/');
+  }, (4* 60 * 1000));  
 };

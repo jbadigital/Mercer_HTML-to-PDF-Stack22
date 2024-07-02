d62aba445efe55b45400ac2286bcfbe142a3ff49
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2024-07-02 13:14:37 +1000

    Update toSFTP.js

--- src/services/docrapter/api/toSFTP.js
+++ src/services/docrapter/api/toSFTP.js
@@ -4,9 +4,6 @@ const axios = require('axios');
 const logger = require('./../../../logger');
 const ET_Client = require('sfmc-fuelsdk-node');
 const { DateTime } = require('luxon');
-//const SFTP = require('ssh2-sftp-client');
-//const sftp = new SFTP();
-
 
 module.exports = function(context) {
 
@@ -14,21 +11,14 @@ module.exports = function(context) {
 
   (async function () {
     try {
-      console.log('step 1 - start');
+      
       const pdf = await axios({
         method: 'get',
         url: context.result.download_url,
         responseType: 'arraybuffer',
         responseEncoding: 'binary'
       });
-      //console.log ( typeof pdf.data.toString() ==='string' ? 'step 1 - result: '+ pdf.data.toString().length : 'step 1 - result: not a atring ' );
-
-      //var test=pdf.data.toString('utf8');
-      //console.log ( test ? 'step 1 - result: '+ pdf.data.toString('utf8').length : 'step 1 - result: not a string ' );
       
-      //console.log (context.result.download_url);
-      
-      console.log('step 1 - finish');
       const client = new ET_Client(process.env.SFMC_CLIENTID, process.env.SFMC_CLIENTSECRET, null, { origin: process.env.SFMC_ORIGIN, authOrigin: process.env.SFMC_AUTHORIGIN, soapOrigin: process.env.SFMC_SOAPORIGIN, authOptions: { authVersion : 2, accountId : process.env.SFMC_ACCOUNTID, scope : process.env.SFMC_SCOPE, applicationType : 'server'}});
       const props = ['PDF_Status','Communication_Name','BRAND_TYPE','Interaction_Name','GUID','PDF_Document_Name'];
       const filter = {
@@ -36,17 +26,11 @@ module.exports = function(context) {
         operator: 'equals',
         rightOperand: context.result.download_id
       }; 
-      console.log('step 2 - start');
-      client.dataExtensionRow({props, Name: 'HTML_to_PDF_Send_Log_V2', filter}).get((err, response) => {
-      //client.dataExtensionRow({props, Name: 'Master_Send_Log_V2_UAT - 20201007', filter}).get((err, response) => {
-        //console.log(filter);
-        //console.log(response);
+      
+      client.dataExtensionRow({props, Name: 'HTML_to_PDF_Send_Log_V2', filter}).get((err, response) => {    
 
         if (err) { console.log("Step - 2 Error:", err); } 
-        console.log("Step - 2 filter: ", filter);
-        console.log("Step - 2 response: ", response.body.Results[0].Properties.Property);
-        
-        console.log('step 2 - finish');
+       
         if (err) throw new Error(err);
 
         let SFMC={};
@@ -59,47 +43,24 @@ module.exports = function(context) {
         SFMC.PDF_Document_Name = _.find(response.body.Results[0].Properties.Property,function(item){ return item.Name === 'PDF_Document_Name'; }).Value;
 
         var dt = DateTime.local();
-        //let filename=SFMC.Communication_Name+'_'+context.result.download_id+'_'+dt.toFormat('yyyy-MM-dd')+'T'+dt.toFormat('HH-mm-ss')+'.pdf';
 
-        //let filename = SFMC.Brand_Type + '-' + SFMC.Interaction_Name + '-' + dt.toFormat('yyyy-MM-dd')+'T'+dt.toFormat('HH-mm-ss') + '-' + SFMC.GUID_Code + '.pdf';
         let filename = SFMC.PDF_Document_Name;
 
         (async function () {
-          try {
-            //console.log(context.app.settings.printmatrix[SFMC.Communication_Name].Destination_Directory+filename);
-            console.log('step 3 - start');
-
-           /* await sftp.connect({
-            host: process.env.FTP_HOST,
-            port: process.env.FTP_PORT,
-            username: process.env.FTP_USERNAME,
-            password: process.env.FTP_PASSWORD,
-            debug: console.log,
-            algorithms: {
-              serverHostKey: ['ssh-rsa', 'ssh-dss']
-            },
-            readyTimeout: 10000000
-            });*/
-           
-
-            
-            //await context.app.settings.sftp.put(Buffer.from(pdf.data), context.app.settings.printmatrix[SFMC.Communication_Name].Destination_Directory+filename);
-            await context.app.settings.sftp.put(Buffer.from(pdf.data), '/Html2PDF/Input/'+filename);
-            //await sftp.end();
-            console.log('step 3 - finish');
+          try {               
+            await context.app.settings.sftp.put(Buffer.from(pdf.data), '/Html2PDF/Input/'+filename);              
             const keyField = {Name: 'PDF_Status', FieldType: 'Text', IsPrimaryKey: false, IsRequired: false, MaxLength: 100};
             const props={};
             props.PDF_Status=context.result.download_id;
             props.PDF_Print_Processed = 1;
             props.PDF_Document_Name = filename;
             //props.PDF_Print_Processed_Date_AEST_2 = dt;
-            console.log('step 4 - start');
+            
             client.dataExtensionRow({props,keyField,Name: 'HTML to PDF - Status Log'}).patch((err, response) => {
 
               if (err) throw new Error(err);
 
             });
-            console.log('step 4 - finish');
 
           } catch (err) {
             logger.error(err);

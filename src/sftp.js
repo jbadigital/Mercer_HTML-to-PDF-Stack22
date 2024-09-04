fdd51c425a95e5b73962bb8be68d25ea1601b633
DaniMekhaeel <73228681+DaniMekhaeel@users.noreply.github.com>
2024-09-04 13:34:57 +1000

    Update sftp.js

--- src/sftp.js
+++ src/sftp.js
@@ -24,7 +24,7 @@ module.exports = async function (app) {
 
   await sftp.connect({
     host: process.env.FTP_HOST,
-    port: 22,
+    port: 2222,
     username: process.env.FTP_USERNAME,
     password: process.env.FTP_PASSWORD,
     //debug: console.log,
